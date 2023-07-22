f357c985c0bdcef623fa8fc07abb5281510b76ff
Fabio Chen <59851467+fabioyebchen@users.noreply.github.com>
2023-07-22 18:18:00 -0400

    a3 until destructing

--- src/labs/lab3/working-with-functions.js
+++ src/labs/lab3/working-with-functions.js
@@ -1,13 +1,20 @@
+import House from "./House";
+import Spread from "./Spread";
 import AddingAndRemovingDataToFromArrays from "./adding-and-removing-data-to-from-arrays";
 import ArrayIndexAndLength from "./array-index-and-length";
 import ArrowFunctions from "./arrow-functions";
+import Destructing from "./destructing";
 import ES5Functions from "./es5-functions";
+import FilterFunction from "./filter-function";
 import FindFunction from "./find-function";
+import FindIndex from "./find-index";
 import ForLoops from "./for-loops";
+import FunctionDestructing from "./function-destructing";
 import FunctionParenthesisAndParameters from "./function-parenthesis-and-parameters";
 import ImpliedReturn from "./implied-return";
 import JSONStringify from "./json-stringify";
 import MapFunction from "./map-function";
+import TemplateLiterals from "./template-literals";
 import WorkingWithArrays from "./working-with-arrays";
 
 function WorkingWithFunctions() {
@@ -25,6 +32,13 @@ function WorkingWithFunctions() {
         <MapFunction/><br/>
         <JSONStringify/><br/>
         <FindFunction/> <br />
+        <FindIndex/> <br />
+        <FilterFunction/> <br />
+        <TemplateLiterals/> <br />
+        <House/> <br />
+        <Spread/> <br />
+        <Destructing/> <br />
+        <FunctionDestructing/> <br />
     </div>
   );
 }
